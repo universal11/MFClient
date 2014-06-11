374964ad611e847f351422017fafb06c0bbe4991
Marc Martinez <universal11@Marcs-MacBook-Pro.local>
2014-06-11 07:55:37 -0700

    Added files

---
+++ MFClient.swift
@@ -0,0 +1,13 @@
+//
+//  MFClient.swift
+//  MFClient
+//
+//  Created by Marc Martinez on 6/11/14.
+//  Copyright (c) 2014 the universal framework. All rights reserved.
+//
+
+import Foundation
+
+class MFClient{
+    
+}
